3fdda52c93c3aeb9e7e73122e416f7eb4576c3f4
Mahbod <mahbod4782@gmail.com>
2021-04-14 22:40:54 +0430

    change base_url eslint error

--- src/api/songs.js
+++ src/api/songs.js
@@ -1,14 +1,14 @@
 // const _songs = []
-const base_url = "https://srv2.myren.xyz/"
+const baseUrl = "https://srv2.myren.xyz/"
 
 export default {
   async getSongs (cb) {
-    const res = await fetch(`${base_url}?req=songs-list`)
+    const res = await fetch(`${baseUrl}?req=songs-list`)
     const songs = res.json()
     cb(songs)
   },
   async getFirstSong (cb) {
-    const res = await fetch(`${base_url}?req=one-song`)
+    const res = await fetch(`${baseUrl}?req=one-song`)
     const song = await res.json()
     cb(song)
   }

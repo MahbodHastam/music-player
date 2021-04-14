2b1a21c697dbc5dba4163c07ac94e8be360a88db
Mahbod <mahbod4782@gmail.com>
2021-04-14 22:36:10 +0430

    change api base_url

--- src/api/songs.js
+++ src/api/songs.js
@@ -1,13 +1,14 @@
 // const _songs = []
+const base_url = "https://srv2.myren.xyz/"
 
 export default {
   async getSongs (cb) {
-    const res = await fetch('http://localhost:9090/?req=songs-list')
+    const res = await fetch(`${base_url}?req=songs-list`)
     const songs = res.json()
     cb(songs)
   },
   async getFirstSong (cb) {
-    const res = await fetch('http://localhost:9090/?req=one-song')
+    const res = await fetch(`${base_url}?req=one-song`)
     const song = await res.json()
     cb(song)
   }
